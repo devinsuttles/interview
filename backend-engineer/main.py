3690122df9946fbb816ad98b98cc91ab01f094da
DevinSuttles <devinfnsuttles@gmail.com>
2021-06-08 11:17:02 -0500

    Verify user provides a valid text file

--- backend-engineer/main.py
+++ backend-engineer/main.py
@@ -2,7 +2,10 @@ from Organize import Organize
 import sys
 
 if __name__ == '__main__':
-    file_name = sys.argv[1]
+    if len(sys.argv) == 1:
+        file_name = input("Please provide file name: ")
+    else:
+        file_name = sys.argv[1]
     try:
         f = open(file_name, 'r')
         lines = f.readlines()
